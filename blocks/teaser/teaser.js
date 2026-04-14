3aece12085a4987972018906eedbbf8e5f3cbfb8
aron.delcarmen@datacom.com <aron.delcarmen@datacom.com>
2026-04-14 11:09:15 +0800

    Fix teaseer lint error

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,5 +1,5 @@
-import { createOptimizedPicture } from "../../scripts/aem.js";
-import { moveInstrumentation } from "../../scripts/scripts.js";
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   const rows = [...block.children];
@@ -7,26 +7,26 @@ export default function decorate(block) {
   const contentRow = rows[1];
 
   // Build image container
-  const imageContainer = document.createElement("div");
-  imageContainer.className = "teaser-image";
+  const imageContainer = document.createElement('div');
+  imageContainer.className = 'teaser-image';
   if (imageRow) {
     moveInstrumentation(imageRow, imageContainer);
-    const pic = imageRow.querySelector("picture");
+    const pic = imageRow.querySelector('picture');
     if (pic) {
-      const img = pic.querySelector("img");
+      const img = pic.querySelector('img');
       if (img) {
         const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [
-          { width: "750" },
+          { width: '750' },
         ]);
-        moveInstrumentation(img, optimizedPic.querySelector("img"));
+        moveInstrumentation(img, optimizedPic.querySelector('img'));
         imageContainer.append(optimizedPic);
       }
     }
   }
 
   // Build content container
-  const contentContainer = document.createElement("div");
-  contentContainer.className = "teaser-content";
+  const contentContainer = document.createElement('div');
+  contentContainer.className = 'teaser-content';
   if (contentRow) {
     moveInstrumentation(contentRow, contentContainer);
     while (contentRow.firstElementChild) {
